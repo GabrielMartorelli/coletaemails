295058b1211d98555440b8f4abea6797d8d58018
Martorelli <martorelli_biel@outlook.com>
2022-11-07 16:29:45 -0300

    finished

---
+++ getemails.py
@@ -0,0 +1,28 @@
+import requests
+import re
+import time
+
+
+def GetEmailBySite(SiteList):
+    for i in SiteList:
+        DataSite = requests.get(i)
+        ReturnRegex = re.findall(f"[\w\.-]+@ [\w\.-] +\.\w+", DataSite.text)
+        if ReturnRegex:
+            return (list(set(ReturnRegex)))
+        else:
+            return None
+
+
+sites = ["http://laboratoriohacker.com.br"]
+
+cont_x = 0
+
+try:
+    for x in sites:
+        mails = (GetEmailBySite([x]))
+        if (mails != "None" or mails != None):
+            print(mails)
+            cont_x = cont_x+1
+except:
+    print(x)
+    pass
